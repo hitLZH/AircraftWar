7cfbfc622b4896d9d72a1c17e799bb486c47d177
cncsmonster <cncsmonster@163.com>
2022-05-25 11:09:28 +0800

    1

--- app/src/main/java/com/hitsz/aircraftwar/EasyActivity.java
+++ app/src/main/java/com/hitsz/aircraftwar/EasyActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EasyActivity extends AppCompatActivity {
-
+    //注释
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
