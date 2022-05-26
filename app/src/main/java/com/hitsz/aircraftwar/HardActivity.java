c7d6c172e60f22383d68cde0a80fb808b53baaf0
cncsmonster <cncsmonster@163.com>
2022-05-26 15:57:11 +0800

    222

--- app/src/main/java/com/hitsz/aircraftwar/HardActivity.java
+++ app/src/main/java/com/hitsz/aircraftwar/HardActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HardActivity extends AppCompatActivity {
-
+    //第二次
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/hitsz/aircraftwar/strategy/strategy.java
+++ app/src/main/java/com/hitsz/aircraftwar/strategy/strategy.java
@@ -1,4 +1,15 @@
 package com.hitsz.aircraftwar.strategy;
+import com.hitsz.aircraftwar.basic.*;
+import com.hitsz.aircraftwar.bullet.*;
+import com.hitsz.aircraftwar.factory.*;
+import com.hitsz.aircraftwar.prop.*;
+import com.hitsz.aircraftwar.strategy.*;
+import com.hitsz.aircraftwar.aircraft.*;
 
-public interface strategy {
+import java.util.List;
+
+public interface Strategy {
+    //根据策略生成不同的设计效果
+    public abstract List<BasicBullet> strategyShoot(AbstractAircraft abstractAircraft);
+
 }
